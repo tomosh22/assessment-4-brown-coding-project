4890adcb1096ea0e38cb7b7a4674f41f788123b2
Tom O'Shaughnessy <b9014614@ncl.ac.uk>
2020-03-30 18:08:03 +0100

    fixed MoveChecker.getFinalScore by flipping row and column in cells index

--- Reversi/src/uk/ac/ncl/game/MoveChecker.java
+++ Reversi/src/uk/ac/ncl/game/MoveChecker.java
@@ -107,7 +107,7 @@ public class MoveChecker {
         int darks = 0;
         for (int row = 0; row < BOARD_SIZE; row++) {
             for (int column = 0; column < BOARD_SIZE; column++) {
-                if (this.cells[column][row].getValue() == CellStatus.LIGHT){
+                if (this.cells[row][column].getValue() == CellStatus.LIGHT){
                     lights++;
                 } else if(this.cells[row][column].getValue() == CellStatus.DARK){
                     darks++;
